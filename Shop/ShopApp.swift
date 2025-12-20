4126630cf4a2c8b32cafc3abe7f2f8199ea72e7f
Muhammed Nadeem <muhammednadeem989@gmail.com>
2025-12-20 20:58:28 +0530

    initial commit

---
+++ Shop/ShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopApp.swift
+//  Shop
+//
+//  Created by Muhammed Nadeem on 20/12/25.
+//
+
+import SwiftUI
+
+@main
+struct ShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
